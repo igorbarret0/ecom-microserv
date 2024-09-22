7fb0f392ddad157f1e67b35e203de38751e46ba7
Pichau <igorof06@outlook.com>
2024-09-22 20:09:17 -0300

    Initialize product-service and add product-service configuration file to config server

---
+++ services/product/src/main/java/com/microserv/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.microserv.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
